1248e2e9bbba502fa39faf0178d4233f5f79fd6f
Marcel <Marcelyap31@gmail.com>
2022-10-20 13:53:10 +0800

    message

---
+++ js/apply.js
@@ -0,0 +1,99 @@
+
+let loanDisplay = document.getElementById("dropdownMenuButton1");
+let loanTenureDisplay = document.getElementById("dropdownMenuButton2");
+
+function changeloan1() {
+    loanDisplay.innerText = "Personal Loan";
+    loanDisplay.style.color = "black";
+
+}
+
+function changeloan2() {
+    loanDisplay.innerText = "Business Loan";
+    loanDisplay.style.color = "black";
+
+}
+function changeloan3() {
+    loanDisplay.innerText = "Housing Loan";
+    loanDisplay.style.color = "black";
+
+}
+function changeloan4() {
+    loanDisplay.innerText = "Student Loan";
+    loanDisplay.style.color = "black";
+
+}
+
+function changeloan5() {
+    loanDisplay.innerText = "Car Loan";
+    loanDisplay.style.color = "black";
+
+}
+
+function changeloan6() {
+    loanDisplay.innerText = "Vacation Loan";
+    loanDisplay.style.color = "black";
+
+}
+function changeloan7() {
+    loanDisplay.innerText = "Wedding Loan";
+    loanDisplay.style.color = "black";
+
+}
+function changeloan8() {
+    loanDisplay.innerText = "Renovation Loan";
+    loanDisplay.style.color = "black";
+
+}
+
+function changeloan9() {
+    loanDisplay.innerText = "Others";
+    loanDisplay.style.color = "black";
+
+}
+
+function windowchange() {
+    window.location.href = "index.html"
+}
+
+
+function changeloantenure6months() {
+    loanTenureDisplay.innerText = "6 Months";
+    loanTenureDisplay.style.color = "black";
+}
+function changeloantenure1year() {
+    loanTenureDisplay.innerText = "1 Year";
+    loanTenureDisplay.style.color = "black";
+
+}
+function changeloantenure2years() {
+    loanTenureDisplay.innerText = "2 Years";
+    loanTenureDisplay.style.color = "black";
+
+}
+function changeloantenure3years() {
+    loanTenureDisplay.innerText = "3 Years";
+    loanTenureDisplay.style.color = "black";
+
+}
+function changeloantenure4years() {
+    loanTenureDisplay.innerText = "4 Years";
+    loanTenureDisplay.style.color = "black";
+
+}
+function changeloantenure5years() {
+    loanTenureDisplay.innerText = "5 Years";
+    loanTenureDisplay.style.color = "black";
+
+}
+function changeloantenure10years() {
+    loanTenureDisplay.innerText = "10 Years";
+    loanTenureDisplay.style.color = "black";
+
+}
+function changeloantenureOthers() {
+    loanTenureDisplay.innerText = "Others";
+    loanTenureDisplay.style.color = "black";
+
+}
+
